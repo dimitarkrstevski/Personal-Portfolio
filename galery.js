7b613a9c99b66d4183d3b27b2ebca4b88f29809f
dimitarkrstevski <dimitarkrstevski13@gmail.com>
2021-11-17 15:26:57 +0100

    init

---
+++ galery.js
@@ -0,0 +1,34 @@
+let images=querySelectorAll(".gallery");
+
+
+
+var fullimgbox=document.getElementById("fullimgbox");
+var fullimg=document.getElementById("fullimg");
+
+function openfullimg(pic){
+
+    fullimgbox.style.display="flex";
+    fullimg.src=pic;
+}
+
+function closefullimg() {
+    fullimgbox.style.display="none";
+}
+
+
+let fullimgbox = document.getElementById("fullimgbox");
+ let fullimg = document.getElementById("fullimg");
+ let gallery = document.querySelectorAll(".gallery");
+ function openfullimg(pic) {
+   fullimgbox.style.display = "flex";
+   fullimg.src = pic;
+   let picNum = pic.slice(-5);
+   picNum = picNum.charAt(0); //br na slika
+   document.getElementById("next").addEventListener("click", 
+function () {
+     fullimg.html = `<img src="${picNum +1}.jpg" id="fullimg" />`;
+   });
+ }
+ function closefullimg() {
+   fullimgbox.style.display = "none";
+ }
