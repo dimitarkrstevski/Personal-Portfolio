afd2df81b8a3e2ab0d97c942f62013fdaf872bf4
dimitarkrstevski <dimitarkrstevski13@gmail.com>
2021-12-14 14:11:21 +0100

    init

--- galery.js
+++ galery.js
@@ -1,45 +1,176 @@
-let images=querySelectorAll(".gallery");
+
+
+let like1 = document.getElementById("like1")
+let cnt1 = 0;
+
+like1.addEventListener("click",function(){
+  let like1N = document.getElementById("like1N");
+
+  cnt1+=1;
+  like1N.innerText = cnt1;
+})
+
+let comment1 = document.getElementById("comment1")
+
+comment1.addEventListener("click",function(e){
+  e.preventDefault();
+
+  let post1 = document.getElementById("post1").value
+
+  document.getElementById("allComments1").innerHTML += post1 + "<br>";
+})
 
 
 
-var fullimgbox=document.getElementById("fullimgbox");
-var fullimg=document.getElementById("fullimg");
 
-function openfullimg(pic){
+let like2 = document.getElementById("like2")
+let cnt2 = 0;
 
-    fullimgbox.style.display="flex";
-    fullimg.src=pic;
-}
+like2.addEventListener("click",function(){
+  let like2N = document.getElementById("like2N");
 
-function closefullimg() {
-    fullimgbox.style.display="none";
-}
+  cnt2+=1;
+  like2N.innerText = cnt2;
+})
 
+let comment2 = document.getElementById("comment2")
 
-let fullimgbox = document.getElementById("fullimgbox");
- let fullimg = document.getElementById("fullimg");
- let gallery = document.querySelectorAll(".gallery");
- function openfullimg(pic) {
-   fullimgbox.style.display = "flex";
-   fullimg.src = pic;
-   let picNum = pic.slice(-5);
-   picNum = picNum.charAt(0); //br na slika
-   document.getElementById("next").addEventListener("click", 
-function () {
-     fullimg.html = `<img src="${picNum +1}.jpg" id="fullimg" />`;
-   });
- }
- function closefullimg() {
-   fullimgbox.style.display = "none";
- }
+comment2.addEventListener("click",function(e){
+  e.preventDefault();
+
+  let post2 = document.getElementById("post2").value
+
+  document.getElementById("allComments2").innerHTML += post2 + "<br>";
+})
 
 
- function incrementButton(){
-   var element = document.getElementById('incrementText')
-   var value = element.innerHTML;
 
-   ++value;
+let like3 = document.getElementById("like3")
+let cnt3 = 0;
 
-   console.log(value)
-   document.getElementById('incrementText').innerHTML = value;
- }
+like3.addEventListener("click",function(){
+  let like3N = document.getElementById("like3N");
+
+  cnt3+=1;
+  like3N.innerText = cnt3;
+})
+
+let comment3 = document.getElementById("comment3")
+
+comment3.addEventListener("click",function(e){
+  e.preventDefault();
+
+  let post3 = document.getElementById("post3").value
+
+  document.getElementById("allComments3").innerHTML += post3 + "<br>";
+})
+
+
+let like4 = document.getElementById("like4")
+let cnt4 = 0;
+
+like4.addEventListener("click",function(){
+  let like4N = document.getElementById("like4N");
+
+  cnt4+=1;
+  like4N.innerText = cnt4;
+})
+
+let comment4 = document.getElementById("comment4")
+
+comment4.addEventListener("click",function(e){
+  e.preventDefault();
+
+  let post4 = document.getElementById("post4").value
+
+  document.getElementById("allComments4").innerHTML += post4 + "<br>";
+})
+
+
+
+let like5 = document.getElementById("like5")
+let cnt5 = 0;
+
+like5.addEventListener("click",function(){
+  let like5N = document.getElementById("like5N");
+
+  cnt5+=1;
+  like5N.innerText = cnt5;
+})
+
+let comment5 = document.getElementById("comment5")
+
+comment5.addEventListener("click",function(e){
+  e.preventDefault();
+
+  let post5 = document.getElementById("post5").value
+
+  document.getElementById("allComments5").innerHTML += post5 + "<br>";
+})
+
+
+
+let like7 = document.getElementById("like7")
+let cnt7 = 0;
+
+like7.addEventListener("click",function(){
+  let like7N = document.getElementById("like7N");
+
+  cnt7+=1;
+  like7N.innerText = cnt7;
+})
+
+let comment7 = document.getElementById("comment7")
+
+comment7.addEventListener("click",function(e){
+  e.preventDefault();
+
+  let post7 = document.getElementById("post7").value
+
+  document.getElementById("allComments7").innerHTML += post7 + "<br>";
+})
+
+
+
+let like8 = document.getElementById("like8")
+let cnt8 = 0;
+
+like8.addEventListener("click",function(){
+  let like8N = document.getElementById("like8N");
+
+  cnt8+=1;
+  like8N.innerText = cnt8;
+})
+
+let comment8 = document.getElementById("comment8")
+
+comment8.addEventListener("click",function(e){
+  e.preventDefault();
+
+  let post8 = document.getElementById("post8").value
+
+  document.getElementById("allComments8").innerHTML += post8 + "<br>";
+})
+
+
+
+
+let like9 = document.getElementById("like9")
+let cnt9 = 0;
+
+like9.addEventListener("click",function(){
+  let like9N = document.getElementById("like9N");
+
+  cnt9+=1;
+  like9N.innerText = cnt9;
+})
+
+let comment9 = document.getElementById("comment9")
+
+comment9.addEventListener("click",function(e){
+  e.preventDefault();
+
+  let post9 = document.getElementById("post9").value
+
+  document.getElementById("allComments9").innerHTML += post9 + "<br>";
+})
